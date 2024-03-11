27179442a23bda7da62854c74801cba2cdb7f045
yseo14 <lmslys14@naver.com>
2024-03-12 01:21:23 +0900

    3/12

---
+++ src/main/java/BOJ/sol1000.java
@@ -0,0 +1,22 @@
+package BOJ;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class sol1000 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(Integer.toString(a+b));
+        bw.flush();
+        bw.close();
+    }
+}
