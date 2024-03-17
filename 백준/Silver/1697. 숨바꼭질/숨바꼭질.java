e4f114e55aedd0ec084bf7e0825817d395b9fe29
Yoonseo Lee <101577272+yseo14@users.noreply.github.com>
2024-03-17 16:41:00 +0900

    [Silver I] Title: 숨바꼭질, Time: 180 ms, Memory: 18708 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,53 +1,60 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+
+import java.util.*;
+import java.io.*;
 
 public class Main {
+
     static int N;
     static int K;
-    static int[] check = new int[100001];
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        K = sc.nextInt();
+    //Java는 int형 배열에 초기값으로 0이 들어간다.
+    static int[] visited = new int[100001];
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
 
         if (N == K) {
             System.out.println(0);
         } else {
             bfs(N);
         }
+
     }
 
-    static void bfs(int num) {
+    public static void bfs(int start) {
         Queue<Integer> q = new LinkedList<>();
-        q.add(num);
-        check[num] = 1;
-
+        q.add(start);
+        visited[start] = 1;
         while (!q.isEmpty()) {
-            int temp = q.poll();
+            int idx = q.poll();
 
             for (int i = 0; i < 3; i++) {
                 int next;
-
+                // x-1, x+1, 2*x 세가지 연산을 위한 반복문
                 if (i == 0) {
-                    next = temp + 1;
+                    next = idx - 1;
                 } else if (i == 1) {
-                    next = temp - 1;
+                    next = idx + 1;
                 } else {
-                    next = temp * 2;
+                    next = idx * 2;
                 }
-
                 if (next == K) {
-                    System.out.println(check[temp]);
+                    System.out.println(visited[idx]);
                     return;
                 }
 
-                if (next >= 0 && next < check.length && check[next] == 0) {
+                if ( next >= 0 && next < visited.length&&visited[next] == 0 ) {
                     q.add(next);
-                    check[next] = check[temp] + 1;
+                    visited[next] = visited[idx] + 1;
                 }
             }
+
         }
+
     }
 }
