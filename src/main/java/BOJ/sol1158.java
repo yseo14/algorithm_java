0a86272e79147b4260f2b4f55de8026e890da906
yseo14 <lmslys14@naver.com>
2024-04-12 01:57:46 +0900

    4/11

---
+++ src/main/java/BOJ/sol1158.java
@@ -0,0 +1,41 @@
+package BOJ;
+
+import java.util.*;
+import java.io.*;
+
+
+public class sol1158 {
+
+    private static int N;
+    private static int K;
+    private static Queue<Integer> q = new LinkedList<>();
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        for (int i = 1; i <= N; i++) {
+            q.offer(i);
+        }
+
+        sb.append('<');
+
+        while (!q.isEmpty()) {
+            for (int i = 0; i < K - 1; i++) {
+                q.offer(q.poll());
+            }
+            if (q.size() == 1) {
+                sb.append(q.poll()).append(">");
+            } else {
+                sb.append(q.poll()).append(", ");
+            }
+        }
+        System.out.println(sb);
+    }
+}
---
+++ src/main/java/BOJ/sol2750.java
@@ -0,0 +1,22 @@
+package BOJ;
+
+import java.io.*;
+import java.util.*;
+
+
+public class sol2750 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        for (int i : arr) {
+            System.out.println(i);
+
+        }
+    }
+}
