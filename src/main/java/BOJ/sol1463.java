f903e1f48aac33d310b0032c2c07ac57dce0e3fc
yseo14 <lmslys14@naver.com>
2024-05-13 23:58:13 +0900

    5/13

---
+++ src/main/java/BOJ/sol1463.java
@@ -0,0 +1,30 @@
+package BOJ;
+
+import java.io.*;
+import java.util.*;
+
+public class sol1463 {
+
+    static int n;
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        dp = new int[n+1];
+        dp[1] = 0;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + 1;
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            }
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
+}
