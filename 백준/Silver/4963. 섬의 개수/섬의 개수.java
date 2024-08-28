4e8926f23fad74e4e45302a243b156103fd3dabd
Yoonseo Lee <101577272+yseo14@users.noreply.github.com>
2024-08-28 15:34:53 +0900

    [Silver II] Title: 섬의 개수, Time: 140 ms, Memory: 16004 KB -BaekjoonHub

---
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -0,0 +1,60 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int w, h;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0, -1, 1, -1, 1};
+    static int[] dy = {0, 0, -1, 1, 1, -1, -1, 1};
+    static int count;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            w = Integer.parseInt(st.nextToken());
+            h = Integer.parseInt(st.nextToken());
+
+            if (w == 0 && h == 0) {
+                return;
+            }
+
+            map = new int[h][w];
+            visited = new boolean[h][w];
+
+            for (int i = 0; i < h; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < w; j++) {
+                    map[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+
+            for (int i = 0; i < h; i++) {
+                for (int j = 0; j < w; j++) {
+                    if (map[i][j] == 1 && !visited[i][j]) {
+                        dfs(i, j);
+                        count++;
+                    }
+                }
+            }
+            System.out.println(count);
+            count = 0;
+        }
+    }
+
+    public static void dfs(int x, int y) {
+        visited[x][y] = true;
+        for (int i = 0; i < 8; i++) {
+            int nX = x + dx[i];
+            int nY = y + dy[i];
+            if ((nX >= 0 && nX < h) && (nY >= 0 && nY < w)) {
+                if (map[nX][nY] == 1 && !visited[nX][nY])
+                    dfs(nX, nY);
+            }
+        }
+    }
+}
