93cd41f354fb93bebe1cd96fe7d9e884fb8fc993
Yoonseo Lee <101577272+yseo14@users.noreply.github.com>
2024-12-09 15:43:29 +0900

    [Silver III] Title: N과 M (1), Time: 2016 ms, Memory: 56284 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,43 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n, m;
+    static int[] arr;
+    static boolean[] isUsed;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m + 1];
+        isUsed = new boolean[n + 1];
+
+        func(0);
+    }
+
+    public static void func(int k) {
+        if (k == m) {
+            for (int i = 0; i < m; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            if (!isUsed[i]) {
+                arr[k] = i;
+                isUsed[i] = true;
+                func(k + 1);
+                isUsed[i] = false;
+            }
+        }
+    }
+}
