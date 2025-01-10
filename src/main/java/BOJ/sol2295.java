31e03d48e778a07c32bc2230f6e4d9a7c6f655b5
yseo14 <lmslys14@naver.com>
2025-01-10 17:52:20 +0900

    boj: 2295번 세 수의 합

---
+++ src/main/java/BOJ/sol2295.java
@@ -0,0 +1,42 @@
+package BOJ;
+
+import java.io.*;
+import java.util.*;
+
+public class sol2295 {
+    static int n;
+    static int[] u;
+    static int[] sum;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        u = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            u[i] = Integer.parseInt(br.readLine());
+        }
+
+        sum = new int[n * n];
+        int idx = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                sum[idx++] = u[i] + u[j];
+            }
+        }
+
+        Arrays.sort(sum);
+
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int target = u[i] - u[j];
+                if (Arrays.binarySearch(sum, target) > -1) {
+                    max = Math.max(max, u[i]);
+                }
+            }
+        }
+        System.out.println(max);
+    }
+}
