4178ae228b4c8ef1c65208f4fa389ba0d491790b
yseo14 <lmslys14@naver.com>
2025-01-22 20:55:52 +0900

    boj: N과 M (4)

---
+++ src/main/java/BOJ/sol15652.java
@@ -0,0 +1,36 @@
+package BOJ;
+
+import java.io.*;
+import java.util.*;
+
+public class sol15652 {
+
+    static int n, m;
+    static int[] arr;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+
+        backTracking(1, 0);
+    }
+
+    static void backTracking(int start, int count) {
+        if (count == m) {
+            for (int i = 0; i < m; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+        for (int i = start; i <= n; i++) {
+            arr[count] = i;
+            backTracking(i, count + 1);
+        }
+    }
+}
