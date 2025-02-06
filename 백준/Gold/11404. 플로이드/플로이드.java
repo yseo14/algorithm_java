2391e3965e070d9a725e20e28683183cd7b49169
Yoonseo Lee <101577272+yseo14@users.noreply.github.com>
2025-02-06 16:02:38 +0900

    [Gold IV] Title: 플로이드, Time: 512 ms, Memory: 43796 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n, m;
+    static int[][] map;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+        map = new int[n + 1][n + 1];
+
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            if (map[start][end] != 0) {
+                map[start][end] = Math.min(map[start][end], cost);
+            } else {
+                map[start][end] = cost;
+            }
+        }
+
+        floyd();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void floyd() {
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (map[i][k] == 0 || map[k][j] == 0 || i == j) {
+                        continue;
+                    }
+                    if (map[i][j] == 0) {
+                        map[i][j] = map[i][k] + map[k][j];
+                    } else {
+                        map[i][j] = Math.min(map[i][k] + map[k][j], map[i][j]);
+                    }
+                }
+            }
+        }
+    }
+}
