9026ab509e6d55a36010af14d7a88f1c85d9311a
Yoonseo Lee <101577272+yseo14@users.noreply.github.com>
2025-02-25 14:24:34 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 104 ms, Memory: 14392 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static long[] dp;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        if (n == 1) {
+            System.out.println(1);
+            return;
+        }
+        dp = new long[n + 1];
+        
+        dp[1] = 1;
+        dp[2] = 3;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
+        }
+
+        System.out.println(dp[n]);
+    }
+}
