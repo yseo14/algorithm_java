cbb4b7af1365b977c646471aaf61de4146e87aa7
Yoonseo Lee <101577272+yseo14@users.noreply.github.com>
2025-03-13 00:58:10 +0900

    [Silver II] Title: A → B, Time: 104 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static long a, b;
+    static int result = 1;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        a = Long.parseLong(st.nextToken());
+        b = Long.parseLong(st.nextToken());
+
+        while (a != b) {
+            if (b < a) {
+                result = -1;
+                break;
+            }
+            if (b % 2 == 0) {
+                b /= 2;
+            } else if (b % 10 == 1) {
+                b /= 10;
+            } else {
+                result = -1;
+                break;
+            }
+            result++;
+        }
+        System.out.println(result);
+    }
+}
