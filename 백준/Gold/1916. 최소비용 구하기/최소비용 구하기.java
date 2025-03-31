cb56c0a435ee1b9ac94fe963f1b2ef6594283fea
Yoonseo Lee <101577272+yseo14@users.noreply.github.com>
2025-03-31 22:49:19 +0900

    [Gold V] Title: 최소비용 구하기, Time: 476 ms, Memory: 50916 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -3,65 +3,64 @@ import java.util.*;
 
 public class Main {
     static int n, m;
-    static ArrayList<City>[] graph;
+    static int targetStart, targetEnd;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         n = Integer.parseInt(br.readLine());
         m = Integer.parseInt(br.readLine());
-        graph = new ArrayList[n + 1];
+
+        ArrayList<City>[] graph = new ArrayList[n + 1];
         for (int i = 0; i < n + 1; i++) {
             graph[i] = new ArrayList<>();
         }
 
+        StringTokenizer st;
         for (int i = 0; i < m; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
             int cost = Integer.parseInt(st.nextToken());
             graph[start].add(new City(end, cost));
         }
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int targetStart = Integer.parseInt(st.nextToken());
-        int targetEnd = Integer.parseInt(st.nextToken());
-        int[] dist = dijkstra(n, targetStart);
-        System.out.println(dist[targetEnd]);
+        st = new StringTokenizer(br.readLine());
+        targetStart = Integer.parseInt(st.nextToken());
+        targetEnd = Integer.parseInt(st.nextToken());
+        int[] result = dijkstra(targetStart, graph);
+        System.out.println(result[targetEnd]);
     }
 
-    public static int[] dijkstra(int n, int start) {
-        boolean[] visited = new boolean[n + 1]; //  해당 정점을 방문했는지 확인
-        int[] dist = new int[n + 1];    //  출발지에서 특정 정점까지 최단거리를 저장 및 갱신
+    public static int[] dijkstra(int start, ArrayList<City>[] graph) {
+        boolean[] visited = new boolean[n + 1];
+        int[] dist = new int[n + 1];
         int INF = Integer.MAX_VALUE;
-
         Arrays.fill(dist, INF);
-        dist[start] = 0;    //  출발지까지의 거리는 0임.
+        dist[start] = 0;
 
-        PriorityQueue<City> pq = new PriorityQueue<>(); //  현재 정점과 인접한 도시들을 저장
+        PriorityQueue<City> pq = new PriorityQueue<>();
         pq.offer(new City(start, 0));
 
         while (!pq.isEmpty()) {
-            int curr = pq.poll().num;   //  현재 확인하는 도시 번호
-            if (visited[curr]) {
+            City curr = pq.poll();
+            if (visited[curr.num]) {
                 continue;
             }
-            visited[curr] = true;
-            for (City next : graph[curr]) {
-                if (dist[next.num] > dist[curr] + next.cost) {
-                    dist[next.num] = dist[curr] + next.cost;
+            visited[curr.num] = true;
+            for (City next : graph[curr.num]) {
+                if (dist[next.num] > dist[curr.num] + next.cost) {
+                    dist[next.num] = dist[curr.num] + next.cost;
                     pq.offer(new City(next.num, dist[next.num]));
                 }
             }
         }
-
         return dist;
     }
 
     public static class City implements Comparable<City> {
         int num, cost;
 
-        public City(int num, int cost) {
+        City(int num, int cost) {
             this.num = num;
             this.cost = cost;
         }
