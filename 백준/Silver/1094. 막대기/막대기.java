94a43863937aaada32ce52e33cb33221a8402b41
Yoonseo Lee <101577272+yseo14@users.noreply.github.com>
2025-04-04 10:36:28 +0900

    [Silver V] Title: 막대기, Time: 100 ms, Memory: 14344 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.java
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -1,26 +1,16 @@
 import java.io.*;
 
 public class Main {
-    static int x;
-    static int result = 0;
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        x = Integer.parseInt(br.readLine());
+        int x = Integer.parseInt(br.readLine());
+        int count = 0;
 
-        int exp = 6;
-        int sum = 0;
-        while (true) {
-            if (sum + Math.pow(2, exp) > x) {
-                exp--;
-            } else {
-                sum += (int) Math.pow(2, exp);
-                result++;
-            }
-            if (sum == x) {
-                break;
-            }
+        while (x > 0) {
+            if ((x & 1) == 1) count++; // 하위 비트가 1이면 막대기 하나 필요
+            x >>= 1; // 오른쪽으로 한 칸 시프트 (다음 비트로 이동)
         }
-        System.out.println(result);
+
+        System.out.println(count);
     }
 }
