67e7b1b9c335ab6df3e611d70db157dc08edfa86
Yoonseo Lee <101577272+yseo14@users.noreply.github.com>
2025-04-21 13:44:49 +0900

    [level 3] Title: 네트워크, Time: 0.48 ms, Memory: 92.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    static int[] dx = {0,0,1,-1};
+    static int[] dy = {1,-1,0,0};
+    static boolean[] visited;
+    
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        visited = new boolean[n];
+        
+        for(int i = 0; i < n; i++) {
+            if(!visited[i]){
+                bfs(i, n, computers);
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+    
+    public static void bfs(int start, int n, int[][] computers) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(start);
+        
+        while(!q.isEmpty()) {
+            int curr = q.poll();
+            visited[curr] = true;
+        
+            for(int i = 0; i < n; i++){
+                if(!visited[i] && computers[curr][i] == 1) {
+                    q.offer(i);
+                    visited[i] = true;
+                }
+            }
+        }
+    }
+}
