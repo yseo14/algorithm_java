4de5be3625c106ed820567cd2509434d34e1ba73
Yoonseo Lee <101577272+yseo14@users.noreply.github.com>
2025-04-28 14:45:11 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 1576 ms, Memory: 22004 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.java
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.java
@@ -4,8 +4,8 @@ import java.util.*;
 public class Main {
     static int n, k;
     static LinkedList<Belt> conveyor;
+    static int brokenBelt;
     static int step = 0;
-    static int brokenBelt = 0;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -13,60 +13,70 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
 
-        int size = 2 * n;
         conveyor = new LinkedList<>();
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < 2 * n; i++) {
             int durability = Integer.parseInt(st.nextToken());
-            conveyor.add(new Belt(false, durability));
+            conveyor.add(i, new Belt(false, durability));
         }
 
-        while (k > brokenBelt) {
-            rotateConveyor();
-            moveRobot();
+        while (brokenBelt < k) {
+            step++;
+            rotateBelt();
+            exitRobot();
+            moveRobots();
+            putRobot();
         }
+
         System.out.println(step);
     }
 
-    public static void rotateConveyor() {
-        step++;
+    public static void rotateBelt() {
         conveyor.addFirst(conveyor.removeLast());
-        conveyor.get(n - 1).isRobotOn = false;  //  내리는 위치에 로봇이 있다면 내림
-
-        if (conveyor.get(0).durability > 0) {   //  올리는 위치 내구도 1 이상이면 로봇 올림
-            conveyor.get(0).isRobotOn = true;
-            conveyor.get(0).durability--;
-            if (conveyor.get(0).durability == 0) {
-                brokenBelt++;
-            }
-        }
     }
 
-    public static void moveRobot() {
+    public static void moveRobots() {
         for (int i = n - 2; i > 0; i--) {
             Belt curr = conveyor.get(i);
             Belt next = conveyor.get(i + 1);
-            if (!curr.isRobotOn) {  //  현재 칸에 이동시킬 로봇이 없으면 이동 불가
+            if (!curr.isRobotOn) {
                 continue;
             }
-
-            if (next.isRobotOn || next.durability < 1) {    //  다음 칸에 로봇이 있거나 내구도가 없는 경우 이동 불가
+            if (next.isRobotOn || next.durability < 1) {
                 continue;
             }
-
-            //  로봇 한칸 이동
-            curr.isRobotOn = false;
-            next.isRobotOn = true;
             next.durability--;
+            next.isRobotOn = true;
+            curr.isRobotOn = false;
+
             if (next.durability == 0) {
                 brokenBelt++;
             }
 
-            if (i + 1 == n - 1) {
-                conveyor.get(i + 1).isRobotOn = false;
+            if (i + 1 == n - 1) {   //  로봇이 내리는 위치로 이동하면 즉시 내림
+                next.isRobotOn = false;
             }
         }
     }
+
+    public static void exitRobot() {
+        Belt end = conveyor.get(n - 1);
+        if (end.isRobotOn) {
+            end.isRobotOn = false;
+        }
+    }
+
+    public static void putRobot() {
+        Belt start = conveyor.getFirst();
+        if (start.durability > 0) {
+            start.isRobotOn = true;
+            start.durability--;
+            if (start.durability == 0) {
+                brokenBelt++;
+            }
+        }
+    }
+
 
     public static class Belt {
         boolean isRobotOn;
