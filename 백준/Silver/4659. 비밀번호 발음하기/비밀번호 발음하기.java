596e8e593c086c68612e0d700d537814f93eefab
Yoonseo Lee <101577272+yseo14@users.noreply.github.com>
2025-05-11 01:23:20 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 124 ms, Memory: 15916 KB -BaekjoonHub

---
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Set<Character> vowels = Set.of('a', 'e', 'i', 'o', 'u');
+
+        while (true) {
+            String pwd = br.readLine();
+            if (pwd.equals("end")) {
+                break;
+            }
+
+            boolean hasVowel = false;
+            boolean isAcceptable = true;
+
+            int vowelCount = 0; //  모음 연속 개수
+            int consonantCount = 0; //  자음 연속 개수
+
+            for (int i = 0; i < pwd.length(); i++) {
+                char curr = pwd.charAt(i);
+
+                // 모음 포함 여부
+                if (vowels.contains(curr)) {
+                    hasVowel = true;
+                    vowelCount++;
+                    consonantCount = 0;
+                } else {
+                    consonantCount++;
+                    vowelCount = 0;
+                }
+
+                // 모음 3개 또는 자음 3개 연속 검사
+                if (vowelCount == 3 || consonantCount == 3) {
+                    isAcceptable = false;
+                    break;
+                }
+
+                // 같은 글자 두 번 연속 검사 (단 ee, oo는 허용)
+                if (i > 0) {
+                    char prev = pwd.charAt(i - 1);
+                    if (curr == prev && !(curr == 'e' || curr == 'o')) {
+                        isAcceptable = false;
+                        break;
+                    }
+                }
+            }
+
+            if (hasVowel && isAcceptable) {
+                System.out.println("<" + pwd + "> is acceptable.");
+            } else {
+                System.out.println("<" + pwd + "> is not acceptable.");
+            }
+        }
+    }
+}
