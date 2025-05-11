1e12688ba40d0d514188ce8178f3dd286811296a
Yoonseo Lee <101577272+yseo14@users.noreply.github.com>
2025-05-11 21:07:41 +0900

    [Silver III] Title: 블로그, Time: 312 ms, Memory: 35064 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.java
+++ 백준/Silver/21921. 블로그/블로그.java
@@ -2,43 +2,39 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n, x;
-    static int[] visitor;
-    static ArrayList<Integer> totalList = new ArrayList<>();
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        x = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int x = Integer.parseInt(st.nextToken());
 
-        visitor = new int[n + 1];
+        int[] visitor = new int[n + 1];
         st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= n; i++) {
             visitor[i] = Integer.parseInt(st.nextToken());
         }
 
-        int maxTotal = 0;
+        int sum = 0;
         for (int i = 1; i <= x; i++) {
-            maxTotal += visitor[i];
+            sum += visitor[i];
         }
-        totalList.add(maxTotal);
 
-        int prevTotal = maxTotal;
+        int maxTotal = sum;
+        int count = 1;
+
         for (int i = x + 1; i <= n; i++) {
-            int currTotal = prevTotal - visitor[i - x] + visitor[i];
-            totalList.add(currTotal);
-            maxTotal = Math.max(currTotal, maxTotal);
-            prevTotal = currTotal;
+            sum = sum - visitor[i - x] + visitor[i];
+            if (sum > maxTotal) {
+                maxTotal = sum;
+                count = 1;
+            } else if (sum == maxTotal) {
+                count++;
+            }
         }
+
         if (maxTotal == 0) {
             System.out.println("SAD");
         } else {
-            int count= 0;
-            for (int total : totalList) {
-                if (total == maxTotal) {
-                    count++;
-                }
-            }
             System.out.println(maxTotal);
             System.out.println(count);
         }
