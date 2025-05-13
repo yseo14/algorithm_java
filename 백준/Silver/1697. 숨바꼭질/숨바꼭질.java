7d96ad2dae3988c4d64b9203af2956e60f59b31c
Yoonseo Lee <101577272+yseo14@users.noreply.github.com>
2025-05-13 14:29:07 +0900

    [Silver I] Title: 숨바꼭질, Time: 140 ms, Memory: 19908 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,26 +1,16 @@
 import java.io.*;
 import java.util.*;
 
-/**
- * 큐에 1초만에 갈 수 있는 위치를 넣는다.
- */
 public class Main {
-    static int N, K;
-    static Queue<Integer> q = new LinkedList<>();
-    static int MIN = 0;
-    static int MAX = 100000;
-    static int[] time;
+    static int n, k;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
 
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        time = new int[100001];
-        Arrays.fill(time, -1);
-
-        if (N == K) {
+        if (n == k) {
             System.out.println(0);
         } else {
             bfs();
@@ -28,32 +18,36 @@ public class Main {
     }
 
     public static void bfs() {
-        q.add(N);
-        time[N] = 0;
+        Queue<Integer> q = new LinkedList<>();
+        int[] visited = new int[100001];
+        q.add(n);
+        visited[n] = 1;
+
         while (!q.isEmpty()) {
             int curr = q.poll();
-            int next;
             for (int i = 0; i < 3; i++) {
+                int next = 0;
                 if (i == 0) {
                     next = curr - 1;
-                } else if (i == 1) {
+                }
+                if (i == 1) {
                     next = curr + 1;
-                } else {
+                }
+                if (i == 2) {
                     next = curr * 2;
                 }
-                if (next == K) {
-                    System.out.println(time[curr] + 1);
-                    return;
+
+                if (next < 0 || next > 100000 || visited[next] != 0) {
+                    continue;
                 }
-                if (isRange(next) && time[next] == -1) {
+                if (next == k) {
+                    System.out.println(visited[curr]);
+                    return;
+                } else {
                     q.add(next);
-                    time[next] = time[curr] + 1;
+                    visited[next] = visited[curr] + 1;
                 }
             }
         }
-    }
-
-    public static boolean isRange(int n) {
-        return n >= MIN && n <= MAX;
     }
 }
