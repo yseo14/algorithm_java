1bbf5c05219673431001a5a1dad8eb45d36ae80c
Yoonseo Lee <101577272+yseo14@users.noreply.github.com>
2025-05-15 15:57:05 +0900

    [Silver V] Title: 문서 검색, Time: 100 ms, Memory: 14376 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.java
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -7,40 +7,20 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         doc = br.readLine();
         word = br.readLine();
 
-        char[] docArr = new char[doc.length()];
-        for (int i = 0; i < doc.length(); i++) {
-            docArr[i] = doc.charAt(i);
-        }
-        char[] wordArr = new char[word.length()];
-        for (int i = 0; i < word.length(); i++) {
-            wordArr[i] = word.charAt(i);
-        }
-
-        int result = 0;
-        for (int i = 0; i <= doc.length() - word.length(); i++) {
-            int count = 0;
-            int startIdx = i;
-            while (startIdx <= doc.length() - word.length()) {
-                int same = 0;
-                for (int j = 0; j < word.length(); j++) {
-                    if (docArr[j + startIdx] == (wordArr[j])) {
-                        same++;
-                    } else {
-                        break;
-                    }
-                }
-                if (same == word.length()) {
-                    count++;
-                    startIdx += word.length();
-                } else {
-                    startIdx++;
-                }
+        int count = 0;
+        int startIdx = 0;
+        while (startIdx <= doc.length() - word.length()) {
+            if (doc.startsWith(word, startIdx)) {
+                count++;
+                startIdx += word.length();
+            } else {
+                startIdx++;
             }
-            result = Math.max(result, count);
         }
-        System.out.println(result);
+        System.out.println(count);
     }
 }
