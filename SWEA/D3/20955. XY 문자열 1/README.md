d98c9644fc1e132a6506655f23082c657bf52f6a
Yoonseo Lee <101577272+yseo14@users.noreply.github.com>
2025-05-23 14:07:10 +0900

    [D3] Title: XY 문자열 1, Time: 133 ms, Memory: 78,208 KB -BaekjoonHub

---
+++ SWEA/D3/20955. XY 문자열 1/README.md
@@ -0,0 +1,15 @@
+# [D3] XY 문자열 1 - 20955 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AY_gm8_6NjcDFAVF) 
+
+### 성능 요약
+
+메모리: 78,208 KB, 시간: 133 ms, 코드길이: 833 Bytes
+
+### 제출 일자
+
+2025-05-23 14:07
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/20955. XY 문자열 1/XY 문자열 1.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T;
+		T = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for (int test_case = 1; test_case <= T; test_case++) {
+			String s = br.readLine();
+			String e = br.readLine();
+
+			while (s.length() < e.length()) {
+				if (e.charAt(e.length() - 1) == 'X') {
+					e = e.substring(0, e.length() - 1);
+				}else {
+					e = e.substring(0, e.length() - 1);
+					e = new StringBuilder(e).reverse().toString();
+				}
+			}
+            sb.append("#"+test_case +" ");
+			if(s.equals(e)) {
+				sb.append("Yes").append("\n");
+			}else {
+				sb.append("No").append("\n");
+			}
+		}
+        System.out.println(sb);
+	}
+
+}
