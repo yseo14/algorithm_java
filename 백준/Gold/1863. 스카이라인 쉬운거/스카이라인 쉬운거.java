790119dbd4897c3ea6dccabe700335506f4ef1ff
Yoonseo Lee <101577272+yseo14@users.noreply.github.com>
2025-05-27 09:53:44 +0900

    [Gold IV] Title: 스카이라인 쉬운거, Time: 180 ms, Memory: 19896 KB -BaekjoonHub

--- 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
@@ -3,39 +3,35 @@ import java.util.*;
 
 public class Main {
     static int n;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
 
-        HashSet<Integer> buildings = new HashSet<>();
-
-        int prev = 0;
+        Stack<Integer> stack = new Stack<>();
         int count = 0;
-        for (int i = 1; i <= n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int idx = Integer.parseInt(st.nextToken());
-            int height = Integer.parseInt(st.nextToken());
 
-            if (prev != height) {
-                if (prev < height) {
-                    count++;
-                    buildings.add(height);
-                } else {
-                    Iterator<Integer> it = buildings.iterator();
-                    while (it.hasNext()) {
-                        int b = it.next();
-                        if (b > height) {
-                            it.remove();
-                        }
-                    }
-                    if (!buildings.contains(height) && height != 0) {
-                        count++;
-                        buildings.add(height);
-                    }
-                }
-                prev = height;
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken()); 
+            int h = Integer.parseInt(st.nextToken());
+
+            while (!stack.isEmpty() && stack.peek() > h) {
+                stack.pop();
+                count++;
+            }
+
+            if (stack.isEmpty() || stack.peek() < h) {
+                stack.push(h);
             }
         }
+
+        while (!stack.isEmpty()) {
+            if (stack.pop() != 0) {
+                count++;
+            }
+        }
+
         System.out.println(count);
     }
 }
