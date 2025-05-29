befef75e0f12c4df0dff25f1c1d2b55567cac491
Yoonseo Lee <101577272+yseo14@users.noreply.github.com>
2025-05-29 17:24:22 +0900

    [Gold IV] Title: 좋다, Time: 148 ms, Memory: 14560 KB -BaekjoonHub

---
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static int[] arr;
+    static int answer = 0;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        for (int i = 0; i < n; i++) {
+            func(i, 0, n - 1);
+        }
+        System.out.println(answer);
+    }
+
+    public static void func(int targetIdx, int left, int right) {
+        while (left < right) {
+            if (left == targetIdx) {
+                left++;
+                continue;
+            }
+            if (right == targetIdx) {
+                right--;
+                continue;
+            }
+            int sum = arr[left] + arr[right];
+            if (sum < arr[targetIdx]) {
+                left++;
+            } else if (sum > arr[targetIdx]) {
+                right--;
+            } else {
+                answer++;
+                return;
+            }
+        }
+    }
+}
