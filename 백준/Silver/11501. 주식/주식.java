85bb38151a936ce9bb0ff13580a1e45ba29bf450
Yoonseo Lee <101577272+yseo14@users.noreply.github.com>
2025-06-02 16:26:45 +0900

    [Silver II] Title: 주식, Time: 1060 ms, Memory: 314088 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.java
+++ 백준/Silver/11501. 주식/주식.java
@@ -12,22 +12,20 @@ public class Main {
             int n = Integer.parseInt(br.readLine());
             int[] arr = new int[n];
             StringTokenizer st = new StringTokenizer(br.readLine());
-
             for (int i = 0; i < n; i++) {
                 arr[i] = Integer.parseInt(st.nextToken());
             }
 
-            int maxPrice = 0;
-            long sum = 0;
+            int max = 0;
+            long ans = 0;
             for (int i = n - 1; i >= 0; i--) {
-                if (maxPrice < arr[i]) {
-                    maxPrice = arr[i];
+                if (arr[i] > max) {
+                    max = arr[i];
                 } else {
-                    sum += maxPrice - arr[i];
+                    ans += max - arr[i];
                 }
             }
-
-            System.out.println(sum);
+            System.out.println(ans);
         }
     }
 }
