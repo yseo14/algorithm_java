c171011c8eeb663f7b84d98d678d222abb48e4ce
Yoonseo Lee <101577272+yseo14@users.noreply.github.com>
2025-06-02 16:00:58 +0900

    [Silver I] Title: 문자열 교환, Time: 104 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Silver/1522. 문자열 교환/문자열 교환.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static String str;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        str = br.readLine();
+
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'a') {
+                count++;
+            }
+        }
+
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < str.length(); i++) {
+            int end = i + count;
+            int bCount = 0;
+            for (int j = i; j < end; j++) {
+                int idx = j % str.length();
+                if (str.charAt(idx) == 'b') {
+                    bCount++;
+                }
+            }
+            min = Math.min(min, bCount);
+        }
+        System.out.println(min);
+    }
+}
