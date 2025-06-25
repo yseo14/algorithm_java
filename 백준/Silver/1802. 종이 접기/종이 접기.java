39fa11fdbdaa46346263454693b3cf9141ba5e37
Yoonseo Lee <101577272+yseo14@users.noreply.github.com>
2025-06-26 00:25:11 +0900

    [Silver I] Title: 종이 접기, Time: 112 ms, Memory: 14952 KB -BaekjoonHub

---
+++ 백준/Silver/1802. 종이 접기/종이 접기.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int t;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            String input = br.readLine();
+            char[] arr = input.toCharArray();
+
+            System.out.println(foldable(arr, 0, arr.length - 1) ? "YES" : "NO");
+        }
+    }
+
+    public static boolean foldable(char[] arr, int left, int right) {
+        if (left >= right) {
+            return true;
+        }
+
+        int mid = (left + right) / 2;
+
+        for (int i = 1; i <= mid - left; i++) {
+            if (arr[mid + i] == arr[mid - i]) {
+                return false;
+            }
+        }
+
+        return foldable(arr, left, mid - 1) && foldable(arr, mid + 1, right);
+    }
+
+}
