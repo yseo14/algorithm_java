0cfbfd0de7295b740b3d17df2a94add96587f582
Yoonseo Lee <101577272+yseo14@users.noreply.github.com>
2025-06-26 18:35:06 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 784 ms, Memory: 29396 KB -BaekjoonHub

---
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[][] arr;
+    static int n, m;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n + 1][m + 1];
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int k = Integer.parseInt(br.readLine());
+        while (k-- > 0) {
+            int x1, y1, x2, y2;
+            st = new StringTokenizer(br.readLine());
+            x1 = Integer.parseInt(st.nextToken());
+            y1 = Integer.parseInt(st.nextToken());
+            x2 = Integer.parseInt(st.nextToken());
+            y2 = Integer.parseInt(st.nextToken());
+            System.out.println(func(x1, y1, x2, y2));
+        }
+    }
+
+    public static int func(int x1, int y1, int x2, int y2) {
+        int sum = 0;
+        for (int i = x1; i <= x2; i++) {
+            for (int j = y1; j <= y2; j++) {
+                sum += arr[i][j];
+            }
+        }
+        return sum;
+    }
+}
