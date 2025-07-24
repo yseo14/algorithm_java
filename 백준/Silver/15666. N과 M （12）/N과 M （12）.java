d09fc7e4ed6718d813b12193c4714e847c669b72
Yoonseo Lee <101577272+yseo14@users.noreply.github.com>
2025-07-24 15:02:09 +0900

    [Silver II] Title: N과 M (12), Time: 432 ms, Memory: 52068 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n, m;
+    static int[] arr;
+    static int[] result;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        result = new int[m];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] sorted = Arrays.stream(arr).distinct().sorted().toArray();
+        dfs(sorted, 0, 0);
+    }
+
+    public static void dfs(int[] sorted, int start, int cnt) {
+        if (cnt == m) {
+            for (int i = 0; i < m; i++) {
+                System.out.print(result[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        boolean[] isUsed = new boolean[10000];
+        for (int i = start; i < sorted.length; i++) {
+            if (isUsed[sorted[i]]) {
+                continue;
+
+            }
+            result[cnt] = sorted[i];
+            dfs(sorted, i, cnt + 1);
+
+        }
+    }
+}
