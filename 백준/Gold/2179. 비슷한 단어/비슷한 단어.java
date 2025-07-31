2fbb63c75f23a639c92f89d758423ad708f1381d
Yoonseo Lee <101577272+yseo14@users.noreply.github.com>
2025-07-31 15:49:34 +0900

    [Gold IV] Title: 비슷한 단어, Time: 1784 ms, Memory: 18456 KB -BaekjoonHub

---
+++ 백준/Gold/2179. 비슷한 단어/비슷한 단어.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static String[] words;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        words = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            words[i] = br.readLine();
+        }
+
+        int max = 0;
+        int first = 0;
+        int second = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int count = check(words[i], words[j]);
+                if (count != -1) {
+                    if (max < count) {
+                        max = count;
+                        first = i;
+                        second = j;
+                    }
+                }
+            }
+        }
+
+        System.out.println(words[first]);
+        System.out.println(words[second]);
+    }
+
+    public static int check(String s1, String s2) {
+        int count = 0;
+        int len = Math.min(s1.length(), s2.length());
+        for (int i = 0; i < len; i++) {
+            if (s1.charAt(i) != s2.charAt(i)) break;
+            count++;
+        }
+        if (s1.equals(s2)) {
+            return -1;
+        }
+        return count;
+    }
+}
