3a8ac1c78d94200846a97250ee161c5772bb0be1
Yoonseo Lee <101577272+yseo14@users.noreply.github.com>
2025-08-17 12:21:16 +0900

    [Silver III] Title: 01타일, Time: 120 ms, Memory: 18084 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static int[] dp;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        if (n <= 2) {
+            System.out.println(n);
+            return;
+        }
+        
+        dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 15746;
+        }
+
+        System.out.println(dp[n]);
+    }
+}
