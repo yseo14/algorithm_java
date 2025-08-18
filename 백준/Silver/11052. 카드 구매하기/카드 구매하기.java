5cc284f534d157627023da60c6d275e46aefd24b
Yoonseo Lee <101577272+yseo14@users.noreply.github.com>
2025-08-18 15:10:19 +0900

    [Silver I] Title: 카드 구매하기, Time: 124 ms, Memory: 14516 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static int[] p;
+    static int[] dp;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        p = new int[n + 1];
+        dp = new int[n + 1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            p[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] = Math.max(dp[i], dp[i - j] + p[j]);
+            }
+        }
+        System.out.println(dp[n]);
+    }
+
+}
