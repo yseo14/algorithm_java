18ef32b9632c9fdf335671022f7fedc3f51a7543
Yoonseo Lee <101577272+yseo14@users.noreply.github.com>
2025-10-02 08:40:31 +0900

    [level 2] Title: 구명보트, Time: 11.07 ms, Memory: 58.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+        int left = 0;
+        int right = people.length - 1;
+        int count = 0;
+        while(left <= right) {
+            if(people[left] + people[right] <= limit) {
+                left++;
+            }
+            right--;
+            count++;
+        }
+        
+        return count;
+    }
+}
