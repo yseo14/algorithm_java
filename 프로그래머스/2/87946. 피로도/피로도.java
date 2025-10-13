d879c049ad2a09832927430556f997818c43df84
Yoonseo Lee <101577272+yseo14@users.noreply.github.com>
2025-10-14 08:34:30 +0900

    [level 2] Title: 피로도, Time: 3.00 ms, Memory: 90.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+//  dfs로 완전탐색
+
+class Solution {
+    static int answer = 0;
+    static boolean[] visited;
+    
+    public int solution(int k, int[][] dungeons) {
+        visited = new boolean[dungeons.length];
+        dfs(0, k, dungeons);
+        return answer;
+    }
+    
+    public void dfs(int cnt, int fatigue,int[][] dungeons) {
+        for(int i = 0; i < dungeons.length; i++) {
+            if(visited[i] || dungeons[i][0] > fatigue) {
+                continue;
+            }
+            visited[i] = true;
+            dfs(cnt + 1, fatigue - dungeons[i][1], dungeons);
+            visited[i] = false;
+        }
+        answer = Math.max(answer, cnt);
+    }
+}
